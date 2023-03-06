9ac300ac3cc34cb61c471256b9bcafe6af7d667a
Henry Berman <heberman@calpoly.edu>
2023-03-05 22:50:01 -0800

    Third attempt

--- src/App.js
+++ src/App.js
@@ -17,32 +17,6 @@ function App() {
 
   const [user, setUser] = useState(null);
 
-  const task = async () => {
-    try {
-      var newUser = null;
-      try {
-        const user = "randotron"
-        const pwd = "Berman#45"
-        const response = await axios.post("/auth",
-            JSON.stringify({ user, pwd }),
-            {
-                headers: { 'Content-Type': 'application/json' }
-            }
-        );
-        newUser = response.data['foundUser'];
-      } catch (err) {
-          console.log(err.message);
-      }
-      newUser.cash -= 10.0;
-      const response = await axios.put('/user/randotron', newUser);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  
-  schedule.scheduleJob('0 * * * *', task);
-
   return (
     <div className="App">
         {user == null ? (<LoginHandler setUser={setUser}/>) : (
