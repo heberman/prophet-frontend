cebeb554bddde206ab69e80460b8268d006c3bac
Henry Berman <heberman@calpoly.edu>
2023-03-05 22:47:04 -0800

    Second attempt

--- src/App.js
+++ src/App.js
@@ -9,39 +9,39 @@ import Stats from './Pages/Stats';
 import StockPage from './Pages/StockPage';
 import LoginHandler from './LoginHandler';
 import axios from './api/axios';
-const cron = require('node-cron');
-
-const task = async () => {
-  try {
-    var newUser = null;
-    try {
-      const user = "randotron"
-      const pwd = "Berman#45"
-      const response = await axios.post("/auth",
-          JSON.stringify({ user, pwd }),
-          {
-              headers: { 'Content-Type': 'application/json' }
-          }
-      );
-      newUser = response.data['foundUser'];
-    } catch (err) {
-        console.log(err.message);
-    }
-    newUser.cash -= 10.0;
-    const response = await axios.put('/user/randotron', newUser);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-cron.schedule('0 * * * *', task);
+const schedule = require('node-schedule');
 
 //alpha vantage api key:MG0ID5XPDBCTO9FF
 
 function App() {
 
   const [user, setUser] = useState(null);
+
+  const task = async () => {
+    try {
+      var newUser = null;
+      try {
+        const user = "randotron"
+        const pwd = "Berman#45"
+        const response = await axios.post("/auth",
+            JSON.stringify({ user, pwd }),
+            {
+                headers: { 'Content-Type': 'application/json' }
+            }
+        );
+        newUser = response.data['foundUser'];
+      } catch (err) {
+          console.log(err.message);
+      }
+      newUser.cash -= 10.0;
+      const response = await axios.put('/user/randotron', newUser);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  schedule.scheduleJob('0 * * * *', task);
 
   return (
     <div className="App">
