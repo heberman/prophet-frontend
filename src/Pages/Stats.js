875ca33b3991d5acc2e07a14bcf56bc68e6f60d7
Henry Berman <heberman@calpoly.edu>
2023-03-08 21:43:10 -0800

    Stats chart 1

--- src/Pages/Stats.js
+++ src/Pages/Stats.js
@@ -15,6 +15,16 @@ const Stats = (props) => {
             <div>
                 <h2>{"Total Value: " + funcs.formatPrice(userData.cash + portVal)}</h2>
                 <h2>{"Net Profit: " + funcs.formatPrice(userData.cash + portVal - 10000.00)}</h2>
+                <LineChart width={730} height={320} data={userData.valueData} 
+                    margin={{ top: 0, right: 20, left: 0, bottom: 5 }}>
+                    <XAxis tickMargin={10} minTickGap={30} dataKey="time"/>
+                    <YAxis tick={false} type="number" domain={([dataMin, dataMax]) => {
+                        const x = (dataMax - dataMin) * 0.1;
+                        return [dataMin - x, dataMax + x];
+                    }} />
+                    <Tooltip />
+                    <Line type="linear" dot={false} dataKey="Value" stroke="#8884d8" animationDuration={1200}/>
+                </LineChart>
                 <h3>History</h3>
                 <TradesTable tradesData={userData.trades} />
             </div>}
