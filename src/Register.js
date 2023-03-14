0cb9b7bab55c113048515b1cc08acac50f652914
Henry Berman <heberman@calpoly.edu>
2023-03-13 21:44:51 -0700

    Logout button 2

--- src/Register.js
+++ src/Register.js
@@ -132,7 +132,7 @@ const Register = (props) => {
                     <FontAwesomeIcon icon={faInfoCircle} />
                     8 to 24 characters.<br />
                     Must include uppercase and lowercase letters, a number and a special character.<br />
-                    Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+                    Allowed special characters: !@#$%
                 </p>
 
 
