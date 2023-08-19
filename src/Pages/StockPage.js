0c9249254f5838a621a32eeeae9a47abd12f8f4e
Henry Berman <heberman@calpoly.edu>
2023-08-19 08:45:10 -0700

    Fixing stock page 2

--- src/Pages/StockPage.js
+++ src/Pages/StockPage.js
@@ -45,8 +45,8 @@ const StockPage = (props) => {
         try {
             const trade = { ticker, numShares: num_shares, date: Date(), price: currPrice }
             const response = await axios.put('/user/' + userData.user, { userData, trade });
-            if (response.status == "success") {
-                updateUser(response.newUser);
+            if (response.data.status == "success") {
+                updateUser(response.data.newUser);
                 return response;
             } else {
                 throw Error(response.status);
